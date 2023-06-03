716dfb3f6f95f2c550471faa785a833a8f1a0669
luuhanh <luuhanh0201@gmail.com>
2023-06-03 12:48:49 +0700

    Thêm thư viện useState useEffect, json-server, sửa lại code để work với thư viện

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { render, router } from "~/router"
+import { render, router } from "~/lib"
 import HomePage from "~/components/pages/Home"
 import ProjectPage from "~/components/pages/Project"
 import AboutPage from "~/components/pages/About"
 import SkillsPage from "~/components/pages/Skills"
 import DetailProject from "~/components/pages/Project/DetailProject"
+import ProductAddPage from "~/components/pages/Skills/add.js"
+import ProductEditPage from "~/components/pages/Skills/update.js"
+import Component from "~/components"
 
 const app = document.getElementById('app')
 
-    
+
 // Render content
-router.on('/', () => render(HomePage, app))
-router.on('/home', () => render(HomePage, app))
-router.on('/about', () => render(AboutPage, app))
-router.on('/skills', () => render(SkillsPage, app))
+router.on('/', () => render(()=>Component(HomePage()), app))
+router.on('/home', () => render(()=>Component(HomePage()), app))
+router.on('/about', () => render(()=>Component(AboutPage()), app))
+router.on('/project', () => render(()=>Component(ProjectPage()), app))
 router.on('/project/:id', ({ data }) => render(() => DetailProject(data), app))
-router.on('/project', () => render(ProjectPage, app))
+
+
+router.on('/skills', () => render(()=>Component(SkillsPage()), app))
+router.on('/skills/add', () => render(()=>Component(ProductAddPage()), app))
+router.on("/product/update/:id", ({ data }) => render(() => ProductEditPage(data), app));
 
 router.resolve()
 
 
+fetch("http://localhost:3000/projects")
+.then(data=>data.json(""))
+.then(data=>{
+    console.log(data)
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Navigo from "navigo"
-import Header from "~/components/DefaultLayout/Header"
-import Footer from "~/components/DefaultLayout/Footer"
-import Component from "~/components"
-const router =new Navigo("/",{linksSelector:"a"})
-const render = (content,target) =>{
-    target.innerHTML = Component(Header,content(),Footer)
-}
+// import Navigo from "navigo"
+// import Header from "~/components/DefaultLayout/Header"
+// import Footer from "~/components/DefaultLayout/Footer"
+// import Component from "~/components"
+// const router =new Navigo("/",{linksSelector:"a"})
+// const render = (content,target) =>{
+//     target.innerHTML = Component(Header,content(),Footer)
+// }
 
-export {render,router}
+// export {render,router}
